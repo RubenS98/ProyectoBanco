c75f6441589141e087947555bc6f02746e03b099
RubenS98 <A01021759@itesm.mx>
2018-04-21 20:08:05 -0500

    Cambio a clase fecha para que se puedan crear fechas con dia

--- Fecha.java
+++ Fecha.java
@@ -1,5 +1,6 @@
 //Ruben estuvo aqui ayer y hoy
 public class Fecha{
+    private int dia;
     private int mes;
     private int anno;
 
@@ -11,7 +12,23 @@ public class Fecha{
         anno = anio;
     }
 
+    public Fecha(int dia, int mes, int anio) throws ArithmeticException {
+        if (mes < 1 || mes > 12)
+            throw new ArithmeticException();
+
+        if (dia < 1 || dia > 31)
+            throw new ArithmeticException();
+
+        this.dia=dia;
+        this.mes = mes;
+        anno = anio;
+    }
+
     public String FormatoFecha(){
         return String.format("%02d/%d", mes, anno);
+    }
+
+    public String FormatoFecha2(){
+        return String.format("%02d/%02d/%d", mes, anno);
     }
 }
