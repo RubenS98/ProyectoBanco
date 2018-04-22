8ff5f1e8a45b84760bd2005244e44a8d2913f669
RubenS98 <A01021759@itesm.mx>
2018-04-21 20:09:14 -0500

    Cambio a clase fecha, ahora fechas pueden tener dia

--- Fecha.java
+++ Fecha.java
@@ -1,5 +1,5 @@
-//what
 public class Fecha{
+    private int dia;
     private int mes;
     private int anno;
 
@@ -11,7 +11,23 @@ public class Fecha{
         anno = anio;
     }
 
+    public Fecha(int dia, int mes, int anio) throws ArithmeticException {
+        if (mes < 1 || mes > 12)
+            throw new ArithmeticException();
+
+        if (dia < 1 || dia > 31)
+            throw new ArithmeticException();
+
+        this.dia=dia;
+        this.mes = mes;
+        anno = anio;
+    }
+
     public String FormatoFecha(){
         return String.format("%02d/%d", mes, anno);
+    }
+
+    public String FormatoFecha2(){
+        return String.format("%02d/%02d/%d", mes, anno);
     }
 }
